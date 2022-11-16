8fabfc3913e59362d1d69eca3cc4c4afd4044f53
Jason <jasonli.0616@outlook.com>
2022-11-15 22:25:01 -0500

    Change variable name to clearer name

--- src/main/java/frc/robot/commands/ToggleGear.java
+++ src/main/java/frc/robot/commands/ToggleGear.java
@@ -7,12 +7,12 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 public class ToggleGear extends InstantCommand {
 
     private final DriveTrain m_driveTrain;
-    private DoubleSolenoid.Value m_gear;
+    private DoubleSolenoid.Value m_gearState;
 
     public ToggleGear(DriveTrain driveTrain) {
 
         m_driveTrain = driveTrain;
-        m_gear = m_driveTrain.getGear();
+        m_gearState = m_driveTrain.getGear();
 
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(driveTrain);
@@ -23,7 +23,7 @@ public class ToggleGear extends InstantCommand {
     @Override
     public void initialize() {
 
-        switch (m_gear) {
+        switch (m_gearState) {
             case kForward:
                 m_driveTrain.changeGear(DoubleSolenoid.Value.kReverse);
                 break;

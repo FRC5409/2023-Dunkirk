9098beeded7bd6912cf3e07ccb4cc7dda73e389b
Jason Li <jasonli.0616@outlook.com>
2022-12-02 09:40:03 -0500

    Switch to Shuffleboard
    Affects: DriveTrain, Gyro, Intake, Pneumatics subsystems

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -7,7 +7,6 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import frc.robot.Constants;
 
 public class Pneumatics extends SubsystemBase {

6bd09d308453e5d232deae605688f53c8f243f81
alexanderszura <1szuraale@hdsb.ca>
2022-12-07 10:46:55 -0500

    Lots of improvements to the shooter
    -Added a limelight subsystem not currently running
    -Fully working PID with potentially working feeding
    -Doing one final test with PID after

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 // import com.playingwithfusion.TimeOfFlight;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;

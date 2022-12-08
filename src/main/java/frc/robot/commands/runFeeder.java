b499c4faca1ff709b2b0bde09743ef3b33958c38
alexanderszura <1szuraale@hdsb.ca>
2022-12-08 17:33:03 -0500

    Working PID stuff need to tune

---
+++ src/main/java/frc/robot/commands/runFeeder.java
@@ -0,0 +1,54 @@
+package frc.robot.commands;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Feeder;
+
+public class runFeeder extends CommandBase {
+
+    private final Feeder m_feeder;
+    private final XboxController m_joystick;
+
+    private ShuffleboardTab tab = Shuffleboard.getTab("Feeder");
+    private NetworkTableEntry kP = tab.add("kP", 0).getEntry();
+    private NetworkTableEntry kI = tab.add("kI", 0).getEntry();
+    private NetworkTableEntry kD = tab.add("kD", 0).getEntry();
+    private NetworkTableEntry kF = tab.add("kF", 0).getEntry();
+
+    public runFeeder(Feeder feeder, XboxController joystick) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        m_feeder = feeder;
+        m_joystick = joystick;
+        addRequirements(m_feeder);
+        
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_feeder.configPIDF(kP.getDouble(0), kI.getDouble(0), kD.getDouble(0), kF.getDouble(0));
+        m_feeder.feed();
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        m_feeder.stopFeeding();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return m_joystick.getAButtonReleased();
+    }
+
+}
--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,25 +1,41 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.kFeeder;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase {
 
     private final CANSparkMax feederMot;
+    private final SparkMaxPIDController pidController;
 
     private boolean isFeeding = false;
 
     public Feeder() {
         feederMot = new CANSparkMax(kFeeder.feederID, MotorType.kBrushless);
+        pidController = feederMot.getPIDController();
         configMot();
         stopFeeding();
     }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+        
+    }
+
+    public void configPIDF(double kP, double kI, double kD, double kF) {
+        pidController.setP(kP);
+        pidController.setI(kI);
+        pidController.setD(kD);
+        pidController.setFF(kF);
+        pidController.setOutputRange(-1, 1);
+    }
 
     @Override
     public void simulationPeriodic() {}
@@ -36,9 +52,10 @@ public class Feeder extends SubsystemBase {
     public void feed() {
         if (!isFeeding) {
             isFeeding = true;
-            feederMot.set(kFeeder.feedSpeed);
+            pidController.setReference(kFeeder.feedSpeed, CANSparkMax.ControlType.kVelocity);
+            // feederMot.set(kFeeder.feedSpeed);
         }
-    }
+    }//
 
     public void stopFeeding() {
         if (isFeeding) {

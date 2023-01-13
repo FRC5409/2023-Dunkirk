a464c0651fba4e225720623864a00a6578228ba0
Ven <92817078+AvianSilk@users.noreply.github.com>
2023-01-12 20:48:53 -0500

    Adding beginning variables and setting up

---
+++ src/main/java/frc/robot/subsystems/Rotate.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Rotate extends SubsystemBase {
+  private final CANSparkMax m_Rotateneo;
+ 
+
+  /** Creates a new ExampleSubsystem. */
+  public Rotate() {
+    m_Rotateneo = new CANSparkMax(Constants.kRotate.kTurrentNeoID,Constants.kRotate.kTurrentNeoModuleType);
+    m_Rotateneo.setIdleMode(null);
+    m_Rotateneo.setInverted(true);
+    m_
+
+
+
+
+  }
+
+  public void disable(){}
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public CommandBase exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+
+  public void rotateLeft (){
+
+
+  }
+
+
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}

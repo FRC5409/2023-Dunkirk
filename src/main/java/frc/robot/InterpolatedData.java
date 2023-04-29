39e6406e4f69abe87c9bb548697969d2559abdae
alexanderszura <1szuraale@hdsb.ca>
2023-04-29 14:08:23 -0400

    Changed to static

--- src/main/java/frc/robot/InterpolatedData.java
+++ src/main/java/frc/robot/InterpolatedData.java
@@ -8,7 +8,7 @@ public final class InterpolatedData {
      * @param steps how many steps is each point
      * @return index of the closest point - 1
      */
-    public int closestPoint(double value, int steps) {
+    public static int closestPoint(double value, int steps) {
         return (int) Math.round(value / steps) - 1;
     }
 
@@ -21,7 +21,7 @@ public final class InterpolatedData {
      * @param x
      * @return New interpolated data
      */
-    public double getInterpolatedSpeed(double x1, double y1, double x2, double y2, double x) {//gets the new interpolated speed
+    public static double getInterpolatedSpeed(double x1, double y1, double x2, double y2, double x) {//gets the new interpolated speed
         // Y = ( ( X - X1 )( Y2 - Y1) / ( X2 - X1) ) + Y1
         // Y: finding interpolated Y value
         // X: Target X cordinant
